cfcf70d5e0d23ee8fe82b166525e9956550fa84c
Marton Suranyi <marton.suranyi@gmail.com>
2012-05-04 08:36:21 +0200

    pure rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coworking::Application.config.secret_token = '78cc9b17c134add05bd5a1d05b45b818c49603227d8b3017316c5838dc23a5424f254e559f9fed5632a535b301ff21c932dfb82fca1aed58e21c9f0028bafa82'
