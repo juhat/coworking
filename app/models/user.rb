01a5e9df2423b7e8ba820261ce75b13b8e3e9ae6
Gabor Laszlo Zelei <zelgala89@gmail.com>
2012-05-11 11:23:06 +0200

    twitter login is ready

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable#, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
@@ -12,10 +12,12 @@ class User < ActiveRecord::Base
 
   def self.find_for_twitter_oauth(access_token, signed_in_resource=nil)
 	data = access_token.extra.raw_info
-	if user = self.find_by_email(data.email)
+	email = "#{data["id"]}@test.com"
+	logger.info email
+	if user = self.find_by_email(email)
 	  user
 	else # Create a user with a stub password. 
-	  self.create!(:email => data.email, :password => Devise.friendly_token[0,20]) 
+	  self.create!(email: email , password: Devise.friendly_token[0,20]) 
 	end
   end
 	def self.new_with_session(params, session)
